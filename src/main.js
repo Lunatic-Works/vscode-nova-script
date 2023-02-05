6e9ebdf95c571e17a28ed66f4b951852f333358a
Kanglai Qian <qiankanglai@gmail.com>
2023-02-05 21:36:53 +0800

    从json里读取api后hover显示

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const vscode = require('vscode');
+const jump_to_definition = require('./jump_to_definition.js');
+const provideHover = require('./hover.js');
+
+exports.activate = function(context) {
+    context.subscriptions.push(vscode.languages.registerDefinitionProvider(['novascript'], new jump_to_definition(context)));
+    context.subscriptions.push(vscode.languages.registerHoverProvider(['novascript'], new provideHover(context)));
+};
